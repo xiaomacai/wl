b69f42cb6c558d550550211beb181492687aabcf
xiaomacai <xmc1028@gmail.com>
2017-02-27 11:52:36 +0800

    add opt loads and nodes route

--- app/map/views.py
+++ app/map/views.py
@@ -8,6 +8,16 @@ def nodes():
     return render_template('map/nodes.html')
 
 
+@map.route('/opt_nodes')
+def opt_nodes():
+    return render_template('map.opt_nodes.html')
+
+
 @map.route('/loads')
 def loads():
     return render_template('map/loads.html')
+
+
+@map.route('/opt_loads')
+def opt_loads():
+    return render_template('map/opt_loads.html')
